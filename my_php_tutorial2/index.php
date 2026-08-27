a9262cac84fe8c5179f6535dc93699f970c93138
umairah24 <nurumairah.ahmadfikri@student.gmi.edu.my>
2026-08-27 21:59:18 +0800

    Add files via upload

---
+++ my_php_tutorial2/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  
+<?php include('header.php'); ?>
+
+<?php include('footer.php'); ?>
+
+
+</html>
+
+<!-- Get & post
+-both methods can be use to send data to ever
+-GET sends the data in the url
+-POST sends the data in the request header(hidden) -->
+
+<!-- //isset = isset check wether a certain variable or value has been set  -->
+
+//form validation
+//meaning - validate the data that user enters into the form after they click submit
+//validate mean prove
